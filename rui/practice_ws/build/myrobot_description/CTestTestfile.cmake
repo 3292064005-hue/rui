7440c453ef9292b8a6da6eb7a3c966d744097d1f
cs <3292064005@qq.com>
2026-06-09 10:23:17 +0000

    。

--- rui/practice_ws/build/myrobot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/workspace/rui/rui/practice_ws/src/myrobot_description
-# Build directory: /root/workspace/rui/rui/practice_ws/build/myrobot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

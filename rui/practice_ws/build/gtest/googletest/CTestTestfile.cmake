7fa01bbb266b4f89485bea987f5c5e021575d86c
cs <3292064005@qq.com>
2026-06-09 11:53:35 +0000

    .

--- rui/practice_ws/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /root/workspace/rui/rui/practice_ws/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
